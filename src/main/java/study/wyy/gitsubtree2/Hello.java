fdf81039d8fa07250b7e1bb018800bc7ea5f1b74
wangyaoyao <wyy226603@alibaba-inc.com>
2019-12-16 08:54:09 +0800

    first commit

---
+++ src/main/java/study/wyy/gitsubtree2/Hello.java
@@ -0,0 +1,12 @@
+package study.wyy.gitsubtree2;
+
+/**
+ * @author wyaoyao
+ * @data 2019-12-16 08:51
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
