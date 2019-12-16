9262cc6243d4e3c809e71021acc1c31c29147c69
wangyaoyao <wyy226603@alibaba-inc.com>
2019-12-16 09:17:01 +0800

    修改。。。

--- src/main/java/study/wyy/gitsubtree2/Hello.java
+++ src/main/java/study/wyy/gitsubtree2/Hello.java
@@ -9,4 +9,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public void test(){
+        System.out.println("修改。。。。");
+    }
 }
